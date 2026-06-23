445002039cb85825864166a018b70b4cb47c7df0
Niraj Gupta <nirajgupta1292003@gmail.com>
2026-06-24 05:19:35 +0530

    Integrated a database

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,5 +1,7 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -7,13 +9,57 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
 
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(
             topics = "${general.kafka-topic}",
             groupId = "midas-group"
     )
     public void listen(Transaction transaction) {
+         System.out.println(
 
-        System.out.println(transaction);
+            "Sender=" + transaction.getSenderId()
 
+            + ", Recipient=" + transaction.getRecipientId()
+
+            + ", Amount=" + transaction.getAmount()
+
+    );
+
+
+        UserRecord sender =
+                databaseConduit.findUser(transaction.getSenderId());
+
+        UserRecord recipient =
+                databaseConduit.findUser(transaction.getRecipientId());
+
+        if (sender == null || recipient == null)
+            return;
+
+        if (sender.getBalance() < transaction.getAmount())
+            return;
+
+        sender.setBalance(
+                sender.getBalance() - transaction.getAmount()
+        );
+
+        recipient.setBalance(
+                recipient.getBalance() + transaction.getAmount()
+        );
+
+        databaseConduit.save(sender);
+        databaseConduit.save(recipient);
+
+        databaseConduit.saveTransaction(
+                new TransactionRecord(
+                        sender,
+                        recipient,
+                        transaction.getAmount()
+                )
+        );
     }
 }
--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -1,5 +1,8 @@
+
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.component.DatabaseConduit;
+import com.jpmc.midascore.entity.UserRecord;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +13,17 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka(
+        partitions = 1,
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092",
+                "port=9092"
+        }
+)
 public class TaskThreeTests {
-    static final Logger logger = LoggerFactory.getLogger(TaskThreeTests.class);
+
+    static final Logger logger =
+            LoggerFactory.getLogger(TaskThreeTests.class);
 
     @Autowired
     private KafkaProducer kafkaProducer;
@@ -23,24 +34,36 @@ public class TaskThreeTests {
     @Autowired
     private FileLoader fileLoader;
 
-    @Test
-    void task_three_verifier() throws InterruptedException {
-        userPopulator.populate();
-        String[] transactionLines = fileLoader.loadStrings("/test_data/mnbvcxz.vbnm");
-        for (String transactionLine : transactionLines) {
-            kafkaProducer.send(transactionLine);
-        }
-        Thread.sleep(2000);
+    @Autowired
+private DatabaseConduit databaseConduit;
 
+@Test
+void task_three_verifier() throws InterruptedException {
 
-        logger.info("----------------------------------------------------------");
-        logger.info("----------------------------------------------------------");
-        logger.info("----------------------------------------------------------");
-        logger.info("use your debugger to find out what waldorf's balance is after all transactions are processed");
-        logger.info("kill this test once you find the answer");
-        while (true) {
-            Thread.sleep(20000);
-            logger.info("...");
+    userPopulator.populate();
+
+    String[] transactionLines =
+            fileLoader.loadStrings("/test_data/mnbvcxz.vbnm");
+
+    for (String transactionLine : transactionLines) {
+        kafkaProducer.send(transactionLine);
+    }
+
+    Thread.sleep(3000);
+
+    System.out.println("\n===== USERS =====");
+
+    for(long id = 1; id <= 20; id++) {
+
+        UserRecord user = databaseConduit.findUser(id);
+
+        if(user != null) {
+            System.out.println(
+                    user.getName()
+                    + " -> "
+                    + user.getBalance()
+            );
         }
     }
+}
 }
